bd1ca4d151fe4f5c6791f25be1072e9cc29a05d9
aishwarya01 <aishwarya.gandhi@fyntune.com>
2021-10-03 16:14:38 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,17 @@
+let a=30;
+ //b=40;
+
+b=60;
+console.log(a);
+console.log(b);
+
+
+//copy by value
+var arr1=[3,4,5,6,7];
+
+var arr2 =arr1; //coping the addres of aaray
+
+arr2[2]=100;
+
+console.log(arr1);
+console.log(arr2);
