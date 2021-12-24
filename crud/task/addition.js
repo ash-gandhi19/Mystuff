6c4d55804cc2e06f04ac7544e3cbc5c663bee940
ash-gandhi19 <ash.gandhi19@gmail.com>
2021-12-24 11:33:41 +0530

    operations using dom

---
+++ crud/task/addition.js
@@ -0,0 +1,14 @@
+let num1=document.getElementById("num1").value;
+let num2=document.getElementById('num2').value;
+console.log(num1);
+console.log("hie");
+// document.getElementById('add').addEventListener('click',additon(num1,num2));
+// document.getElementById('sub').addEventListener('click',sub(num1,num2));
+// document.getElementById('mul').addEventListener('click',mul(num1,num2));
+// document.getElementById('div').addEventListener('click',div(num1,num2));
+
+// let additon=((a,b)=>{
+//     sum = a+b;
+
+//     document.getElementById('printoutput').innerHTML=sum;
+// });
